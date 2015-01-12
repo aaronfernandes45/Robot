d457a4c6b419646def97a382758e3e13ed81b3f4
aaronfernandes45 <aaronfernandes45@gmail.com>
2015-01-12 18:14:50 +0530

    UI added

--- test2.js
+++ test2.js
@@ -12,6 +12,7 @@ function control()
 			var b = prompt("y co ord");
 			player1.add([a,b]);
 			display();
+			mDown();
 			break;
 		}
 		case "2":
@@ -20,11 +21,13 @@ function control()
 			var b = prompt("y co ord");
 			player2.add([a,b]);
 			display();
+			mDown();
 			break;
 		}
 		default:
 		{
 			display();
+			mDown();
 			break;
 		}
 	}
@@ -47,3 +50,53 @@ function display()
 }
 
 control();
+
+
+
+//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+
+function mDown()
+{
+	var c = document.getElementById("myCanvas");
+	var ctx = c.getContext("2d");
+	ctx.font = "12px Ariel";
+	for(var i=0; i<4; i++)
+	{
+		for(var j=0; j<4; j++)
+		{
+			ctx.clearRect((0+(j*100)), (0+(i*100)), 100, 100);
+		}
+	}
+
+	for(var i=0; i<400; i+=100)
+	{
+		ctx.moveTo(0,i);
+		ctx.lineTo(400,i);
+		ctx.stroke();
+	}
+
+	for(var j=0; j<400; j+=100)
+	{
+		ctx.moveTo(j,0);
+		ctx.lineTo(j,400);
+		ctx.stroke();
+	}
+
+	for(var i=0; i<4; i++)
+	{
+		for(var j=0; j<4; j++)
+		{
+			ctx.textAlign = "start";
+			ctx.fillText(grid.gridStructure[i][j].length,(10+(100*j)),((i*100)+50));
+
+			// var popObj = grid.gridStructure[i][j].pop();
+			// if(!popObj)
+			// {
+			// 	ctx.fillText(popObj.color,(10+(100*j)),((i*100)+65));
+			// 	grid.gridStructure[i][j].push(popObj);
+			// }
+		}
+	}
+	console.log("bdhjgde");
+}
